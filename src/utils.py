b741cf2a801f61e32fcaf7ac47634af4552eb670
akshbhalani <akshbhalani119@gmail.com>
2024-12-24 21:31:58 +0100

    Flask application

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 import dill
+import pickle
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 
@@ -47,3 +48,11 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
     
     except Exception as e:
         raise CustomException(e, sys)
+
+
+def load_data(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
